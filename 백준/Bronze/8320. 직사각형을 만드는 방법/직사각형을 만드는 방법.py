1ad6dbbc26926fbb92cddf45aa3ab766894e9980
jinjintaek <tijerjt@gmail.com>
2025-05-21 18:06:40 +0100

    Update 직사각형을 만드는 방법.py
    시간초과했던 답안 주석으로 업데이트

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -9,4 +9,27 @@ for i in range(1, N+1):
         result += 1
         
 print(result)
-       
+
+# ----------- 시간 초과했던 답 --------------
+# N = int(sys.stdin.readline())
+
+# def c_count(N):
+#     count = 0
+    
+#     for i in range(1, int(N**0.5) + 1):
+#         if N % i == 0:
+#             count += 2 if i != N // i else i 
+#     return count
+
+# def solution(N):
+#     result = 0
+#     for i in range(1, N+1):
+#         if c_count(i) % 2 == 0:
+#             result += (c_count(i) // 2)
+#         elif c_count(i) % 2 != 0:
+#             result += ((c_count(i) + 1) // 2)
+            
+#     return result
+    
+
+# print(solution(N))
