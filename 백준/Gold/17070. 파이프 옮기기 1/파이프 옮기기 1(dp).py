f9942850551a36407391382aed90dc0c113971bf
Jintaek Uh <tijerjt@gmail.com>
2025-07-20 06:37:46 +0100

    Create 파이프 옮기기 1(dp).py
    dp버전 추가

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1(dp).py
@@ -0,0 +1,23 @@
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[[0]*3 for _ in range(N)] for _ in range(N)]
+
+dp[0][1][0] = 1
+
+for y in range(N):
+    for x in range(N):
+        if board[y][x] == 1:
+            continue
+        
+        if x - 1 > 0:
+            dp[y][x][0] += dp[y][x-1][0] + dp[y][x-1][2]
+            
+        if y - 1 > 0:
+            dp[y][x][1] += dp[y-1][x][1] + dp[y-1][x][2]
+            
+        if x - 1 >= 0 and y - 1 >= 0:
+            if board[y-1][x] == 0 and board[y][x-1] == 0:
+                dp[y][x][2] += dp[y-1][x-1][0] + dp[y-1][x-1][1] + dp[y-1][x-1][2]
+                
+print(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2])
