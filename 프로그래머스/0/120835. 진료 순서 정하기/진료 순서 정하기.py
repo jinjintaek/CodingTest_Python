73cb6df3c5ca62e82ade39da76966d30c3e31989
Jintaek Uh <tijerjt@gmail.com>
2025-08-19 17:27:53 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,8 +1,6 @@
 def solution(emergency):
-    a = sorted(emergency, reverse=True)
-    order = []
-    for i in emergency:
-        for j in a:
-            if i == j:
-                order.append(a.index(j)+1)
-    return order
+    order = {}
+    for i, v in enumerate(sorted(emergency, reverse=True)):
+        order[v] = i + 1
+    
+    return [order[i] for i in emergency]
