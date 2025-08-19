1261ef416603b1bc56e4a6fa582a7eea9fe06e7b
Jintaek Uh <tijerjt@gmail.com>
2025-08-20 01:20:21 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,9 +2,10 @@ def solution(clothes):
     count = {}
     for name, kind in clothes:
         if kind not in count:
-            count[kind] = 0
-        count[kind] += 1
+            count[kind] = 2
+        else:
+            count[kind] += 1
     ways  = 1
     for cnt in count.values():
-        ways *= (cnt + 1)
+        ways *= cnt
     return ways -1
