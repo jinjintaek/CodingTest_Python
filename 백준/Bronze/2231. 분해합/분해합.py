2a172f42b644d06d723d0953fcc089fce1b46516
Jintaek Uh <tijerjt@gmail.com>
2025-10-14 18:37:03 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,20 +2,10 @@ import sys
 
 N = int(sys.stdin.readline().strip())
 
-def sum_digits(num):
-    digits = []
-    for n in str(num):
-        digits.append(int(n))
-    return sum(digits)
-        
-k = len(str(N))
-
-S = max(1, N - 9*k)
-
 M = 0
-for i in range(S, N):
-    if i + sum_digits(i) == N:
-        M = i
+for i in range(max(0,N-len(str(N))*9),N):
+    if sum(map(int,str(i))) + i == N:
+        M = i 
         break
-        
+
 print(M)
