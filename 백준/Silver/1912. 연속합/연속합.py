c79cfacc3cfbf81dc0a259c9c0b02bc6294957c5
Jintaek Uh <tijerjt@gmail.com>
2026-01-22 02:27:35 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,9 @@
+A = int(input())
+arr = list(map(int, input().split()))
+
+prefix = [0 for _ in range(A+1)]
+
+for i in range(A):
+    prefix[i+1] = max(prefix[i] + arr[i], arr[i])
+
+print(max(prefix[1:]))
