3c89f1022fb1cb5d9d391c24099e74faefc02370
Jintaek Uh <tijerjt@gmail.com>
2026-01-31 21:52:35 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 3.85 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    orders = [sorted(order) for order in orders]
+
+    answer = []
+    for course_size in course:
+        counter = Counter()
+        for order in orders:
+            for comb in combinations(order, course_size):
+                counter[comb] += 1
+
+        if counter:
+            max_count = max(counter.values())
+            if max_count >= 2:
+                for comb, count in counter.items():
+                    if count == max_count:
+                        answer.append(''.join(comb))
+                        
+    return sorted(answer)
