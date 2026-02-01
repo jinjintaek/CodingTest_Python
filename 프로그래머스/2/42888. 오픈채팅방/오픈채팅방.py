aaa134bc0e08e369fa36611b6506170f2e2fddfc
Jintaek Uh <tijerjt@gmail.com>
2026-02-01 13:37:50 +0900

    [level 2] Title: 오픈채팅방, Time: 94.26 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,20 +1,21 @@
 def solution(record):
     
     user2nick = {}
-
-
+    
     for rec in record:
         arr = rec.split()
-        if len(arr) == 3:
+        if len(arr) > 2:
             user2nick[arr[1]] = arr[2]
     
-
     answer = []
+    
     for rec in record:
         arr = rec.split()
-        if arr[0] == 'Enter':
+        if arr[0] == "Enter":
             answer.append(f"{user2nick[arr[1]]}님이 들어왔습니다.")
-        elif arr[0] == 'Leave':
+        elif arr[0] == "Leave":
             answer.append(f"{user2nick[arr[1]]}님이 나갔습니다.")
-
+            
     return answer
+            
+        
