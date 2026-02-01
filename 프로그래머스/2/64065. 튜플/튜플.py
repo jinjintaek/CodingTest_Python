ecfa77d1ee320e7852316ceae9746fb999cd7ba3
Jintaek Uh <tijerjt@gmail.com>
2026-02-01 14:15:38 +0900

    [level 2] Title: 튜플, Time: 318.53 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,16 +1,20 @@
 def solution(s):
     s = s[2:-2]
     s = s.split("},{")
-
+    
     s_list = []
     for i in s:
         s_list.append(i.split(','))
-
+    
     s_list.sort(key=len)
-
+    
     answer = []
+    
     for s in s_list:
         for num in s:
             if int(num) not in answer:
                 answer.append(int(num))
+                
+    
     return answer
+                
