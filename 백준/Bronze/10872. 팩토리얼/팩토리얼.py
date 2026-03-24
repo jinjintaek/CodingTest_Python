2a7ad9ae3bacb9dfcaceff0547805aec705973f5
Jintaek Uh <tijerjt@gmail.com>
2026-03-24 15:54:45 +0900

    [Bronze III] Title: 팩토리얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,7 +1,9 @@
 N = int(input())
 
-total = 1
-for i in range(1,N+1):
-    total = total * i
-
-print(total)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(N))
