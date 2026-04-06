6a3601508beaf2638a05b735fd33a44828baba04
Jintaek Uh <tijerjt@gmail.com>
2026-04-06 21:02:10 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,13 +1,9 @@
-# 입력 받기
-A, B = map(int, input().split())  # 현재 시각 A(시), B(분)
-C = int(input())  # 요리 시간 C(분)
+import sys
 
-# 총 분 계산
-total_minutes = (A * 60 + B) + C
+input = sys.stdin.readline
 
-# 시(hour)와 분(minute) 계산
-hour = (total_minutes // 60) % 24
-minute = total_minutes % 60
+a, b = map(int, input().split())
+c = int(input())
 
-# 결과 출력
-print(hour, minute)
+total = a * 60 + b + c
+print(total // 60 % 24, total % 60)
