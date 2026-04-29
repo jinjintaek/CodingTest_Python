b5be9e4f02b605f6f199bf58634aa511900b5554
Jintaek Uh <tijerjt@gmail.com>
2026-04-29 13:19:08 +0900

    [level 2] Title: 피로도, Time: 38.99 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,23 +1,23 @@
+from itertools import permutations
 def solution(k, dungeons):
+    # 던전 순서 순열 
+    # 던전 방문 최대회수 업데이트
+    # 던전을 못돌면 바로 현재 던전 수 기록
     max_cnt = 0
+    
     for order in permutations(dungeons, len(dungeons)):
-        current_k = k
+        ck = k
         cnt = 0
         for dg in order:
             n = dg[0]
             s = dg[1]
-            if current_k >= n:
-                current_k -= s
+            if ck >= n:
+                ck = ck - s
                 cnt += 1
             else:
                 break
         
         max_cnt = max(max_cnt, cnt)
-    
+        
     return max_cnt
-            
                 
-    
-    return max(answer)
-
-from itertools import permutations
