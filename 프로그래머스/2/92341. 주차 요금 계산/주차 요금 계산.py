924e3bd09b453d1906b4b7b38b4659adb639a4a8
Jintaek Uh <tijerjt@gmail.com>
2026-07-22 23:57:19 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.95 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,35 +1,36 @@
 import math
 
 def solution(fees, records):
-    
-    latest_time = {}
     total_time = {}
-    answer = []
-    
+    current_time = {}
     for record in records:
-        r = record.split()
-        t, n, s = r[0], r[1], r[2]
-        if s == 'IN':
-            latest_time[n] = t
-        if s == 'OUT':
-            gap = tc(t) - tc(latest_time[n])
-            total_time[n] = total_time.get(n, 0) + gap
-            latest_time[n] = None
-    for k, v in latest_time.items():
-        if v != None:
-            gap = tc('23:59') - tc(latest_time[k])
-            total_time[k] = total_time.get(k, 0) + gap
+        t,n,s = record.split()
+        if s == "IN":
+            current_time[n] = tc(t)
+        else:
+            total_time[n] = total_time.get(n, 0) + (tc(t) - current_time[n])
+            current_time[n] = None
             
+    for k, v in current_time.items():
+        if v != None:
+            total_time[k] = total_time.get(k, 0) + (tc("23:59") - v)
+    
     total_time = sorted(total_time.items())
     
-    for tt in total_time:
-        time = tt[1]
-        answer.append(max(math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1], fees[1]))
     
+    
+    answer = []
+    for (k, v) in total_time:
+        if v > fees[0]:
+            answer.append(fees[1]+ math.ceil((v-fees[0])/fees[2]) * fees[3])
+        else:
+            answer.append(fees[1])
+        
     return answer
-    
+
 def tc(time):
-    tt = time.split(':')
-    return int(tt[0]) * 60 + int(tt[1])
-    
+    h, m = time.split(':')
+    return int(h) * 60 + int(m)
+        
+            
         
