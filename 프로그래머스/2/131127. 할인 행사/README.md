660e10b2fa44a9434e4e9e0495e34bad86b0fc69
Jintaek Uh <tijerjt@gmail.com>
2026-07-25 01:00:58 +0900

    [level 2] Title: 할인 행사, Time: 99.75 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 18.4 MB, 시간: 104.07 ms
+메모리: 18.5 MB, 시간: 99.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 25일 00:49:26
+2026년 07월 25일 01:00:56
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,15 +1,15 @@
+from collections import Counter
+
 def solution(want, number, discount):
     answer = 0
-
     
-    for idx in range(len(discount) - 9):
-        current = number[:]
-        
-        for item in discount[idx:idx+10]:
-            if item in want:
-                current[want.index(item)] -= 1
-                
-        if all(cnt <= 0 for cnt in current):
-            answer += 1
+    dic = dict(zip(want,number))
+
+    for i in range(len(discount)):
+        window = discount[i:i+10]
+        if len(window) < 10:
+            break
             
+        if dic == Counter(window):
+            answer += 1
     return answer
