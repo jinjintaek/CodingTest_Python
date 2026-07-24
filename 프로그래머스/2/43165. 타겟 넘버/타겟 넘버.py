df3408f8a9991070655c5aa400121468a940a8c5
Jintaek Uh <tijerjt@gmail.com>
2026-07-25 03:34:36 +0900

    [level 2] Title: 타겟 넘버, Time: 118.19 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,8 @@
 def solution(numbers, target):
-    answer = 0
-    n = len(numbers)
-    
     def dfs(idx, current):
-        nonlocal answer
+        if idx == len(numbers):
+            return 1 if current == target else 0
         
-        if idx == n:
-            if current == target:
-                answer += 1
-            return 
-        
-        dfs(idx+1, current + numbers[idx])
-        dfs(idx+1, current - numbers[idx])
-        
-    dfs(0,0)
-    
-    return answer
+        return dfs(idx + 1, current + numbers[idx]) + dfs(idx + 1, current - numbers[idx])
+    return dfs(0,0)
     
