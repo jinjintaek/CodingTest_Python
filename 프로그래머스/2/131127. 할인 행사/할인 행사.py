f36c5b8a55095ad3f14733e3bbd6327cb5f530eb
Jintaek Uh <tijerjt@gmail.com>
2026-07-25 23:16:15 +0900

    [level 2] Title: 할인 행사, Time: 98.23 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,15 +1,16 @@
 from collections import Counter
-
 def solution(want, number, discount):
     answer = 0
+    dic = {a:b for a, b in zip(want, number)}
     
-    dic = dict(zip(want,number))
-
     for i in range(len(discount)):
-        window = discount[i:i+10]
+        window = discount[i : i + 10]
+        
         if len(window) < 10:
             break
-            
+        
         if dic == Counter(window):
             answer += 1
+    
+    
     return answer
