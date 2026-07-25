de7c2cc0440d8c184c59428335238c2f3086a8a8
Jintaek Uh <tijerjt@gmail.com>
2026-07-25 19:18:44 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,22 @@
 import math
 def solution(progresses, speeds):
-    left = []
-    days = []
-    for per in progresses:
-        left.append(100-per)
-    for per, speed in zip(left, speeds):
-        days.append(math.ceil(per/speed))
     answer = []
-    cur = days[0]
+    left = []
+    
+    for progress, speed in zip(progresses,speeds):
+        left.append(math.ceil((100-progress)/speed))
+        
+    current = left[0]
     cnt = 1
     
-    for d in days[1:]:
-        if d <= cur:
+    for d in left[1:]:
+        if d <= current:
             cnt += 1
         else:
             answer.append(cnt)
-            cur = d
+            current = d
             cnt = 1
-    answer.append(cnt)
-    return answer
     
+    answer.append(cnt)
         
-        
-        
+    return answer
