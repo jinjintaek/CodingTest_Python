a89462bf6ef6c7feaf2ea0039803f8f0b1140420
Jintaek Uh <tijerjt@gmail.com>
2026-07-29 02:42:11 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,7 +1,5 @@
 def solution(arr, idx):
-    answer = 0
-    for i, v in enumerate(arr[idx:]):
-        if v == 1:
-            answer = i + idx
-            return answer
-    return -1
+    try:
+        return arr.index(1, idx)
+    except ValueError:
+        return -1
