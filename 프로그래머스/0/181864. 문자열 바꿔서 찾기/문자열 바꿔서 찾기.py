9d02d73d05327a58f4ee6c9e4fe3741bd0c5f1ae
Jintaek Uh <tijerjt@gmail.com>
2026-08-05 22:42:21 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,6 +1,3 @@
 def solution(myString, pat):
-    myString = myString.replace('A','X').replace('B','A').replace('X','B')
-    if pat in myString:
-        return 1
-    else:
-        return 0
+    answer = 0
+    return answer
