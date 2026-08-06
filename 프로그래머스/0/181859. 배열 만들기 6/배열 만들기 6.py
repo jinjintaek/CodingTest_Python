c4a0aee689dc61f3bc3ce1f99923bbec71a77625
Jintaek Uh <tijerjt@gmail.com>
2026-08-07 02:41:54 +0900

    [level 0] Title: 배열 만들기 6, Time: 71.84 ms, Memory: 19.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    answer = []
+    i = 0
+    while i < len(arr):
+        if not answer:
+            answer.append(arr[i])
+            i += 1
+        elif answer[-1] == arr[i]:
+            answer.pop()
+            i += 1
+        else:
+            answer.append(arr[i])
+            i += 1
+    
+    return answer if answer else [-1]
